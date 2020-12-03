bda5f4da9c73bf06e89d70d18cad08b22ba87650
sjpark1 <sjpark1@nkia.co.kr>
2020-12-03 18:59:27 +0900

    영화 앱 개발 #1

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {AppLoading} from "expo";
+import * as Font from "expo-font";
+import {Image, StatusBar} from "react-native";
+import {Asset} from "expo-asset";
+import {Ionicons} from "@expo/vector-icons";
+import {NavigationContainer} from "@react-navigation/native";
+import Stack from "./navigation/Stack";
+
+const cacheImages = (images) => {
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://unsplash.com/photos/S9zAGWOlwPI",
+      require("./assets/splash.png"),
+    ]);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = () => setIsReady(true);
+
+  return isReady ? (
+    <>
+      <NavigationContainer>
+        <Stack />
+      </NavigationContainer>
+      <StatusBar barStyle={"light-content"} />
+    </>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={(e) => console.error(e)}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation/Stack.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Detail from "../screens/Detail";
+import Tabs from "./Tabs";
+
+// 부모 내바게이션
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator
+    // mode="modal"
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "black",
+        borderBottomColor: "black",
+        shadowColor: "black",
+      },
+      headerTintColor: "white",
+      headerBackTitleVisible: false,
+    }}
+  >
+    <Stack.Screen name="Tabs!" component={Tabs} />
+    <Stack.Screen name="Detail" component={Detail} />
+  </Stack.Navigator>
+);
